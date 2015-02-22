647d03477d63c4c52bd276392211506d27611977
Alex Browne <stephenalexbrowne@gmail.com>
2015-02-21 22:20:33 -0500

    Implement and test a jade compiler

---
+++ compilers/jade_compiler.go
@@ -0,0 +1,108 @@
+package compilers
+
+import (
+	"fmt"
+	"github.com/albrow/scribble/config"
+	"github.com/albrow/scribble/log"
+	"github.com/albrow/scribble/util"
+	"github.com/howeyc/fsnotify"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+// JadeCompilerType represents a type capable of compiling jade files.
+type JadeCompilerType struct {
+	// createdFiles is a slice of file paths which were created by this
+	// compiler. It is important for implementing the RemoveOld method.
+	createdFiles []string
+}
+
+// JadeCompiler is an instatiation of JadeCompilerType
+var JadeCompiler = JadeCompilerType{}
+
+// CompileMatchFunc returns a MatchFunc which will return true for
+// any files which match a given pattern. In this case, the pattern
+// is any file that ends in ".jade", excluding hidden and ignored
+// files and directories.
+func (*JadeCompilerType) CompileMatchFunc() MatchFunc {
+	return filenameMatchFunc("*.jade", true, true)
+}
+
+// WatchMatchFunc returns a MatchFunc which will return true for
+// any files which match a given pattern. In this case, the pattern
+// is any file that ends in ".jade", excluding hidden files and directories,
+// but including those that start with an underscore, since they may
+// be imported in other files.
+func (*JadeCompilerType) WatchMatchFunc() MatchFunc {
+	return filenameMatchFunc("*.jade", true, false)
+}
+
+// Compile compiles the file at srcPath. The caller will only
+// call this function for files which belong to JadeCompiler
+// according to the MatchFunc. Behavior for any other file is
+// undefined. Compile will output the compiled result to the appropriate
+// location in config.DestDir.
+func (j *JadeCompilerType) Compile(srcPath string) error {
+	// parse path and figure out destPath
+	destPath := strings.Replace(srcPath, ".jade", ".html", 1)
+	destPath = strings.Replace(destPath, config.SourceDir, config.DestDir, 1)
+	log.Success.Printf("CREATE: %s -> %s", srcPath, destPath)
+
+	// create the dest directory if needed
+	if err := os.MkdirAll(filepath.Dir(destPath), os.ModePerm); err != nil {
+		// if the dir already exists, that's fine
+		// if there was some other error, return it
+		if !os.IsExist(err) {
+			return err
+		}
+	}
+
+	// set up and execute the command, capturing the output only if there was an error
+	destDir := filepath.Dir(destPath)
+	cmd := exec.Command("jade", srcPath, "--out", destDir)
+	response, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("while compiling jade: %s", string(response))
+	}
+
+	// Add destPath to the list of created files
+	j.createdFiles = append(j.createdFiles, destPath)
+
+	return nil
+}
+
+// CompileAll compiles zero or more files identified by srcPaths.
+// It works simply by calling Compile for each path. The caller is
+// responsible for only passing in files that belong to JadeCompiler
+// according to the MatchFunc. Behavior for any other file is undefined.
+func (j *JadeCompilerType) CompileAll(srcPaths []string) error {
+	log.Default.Println("Compiling jade...")
+	for _, srcPath := range srcPaths {
+		if err := j.Compile(srcPath); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (j *JadeCompilerType) FileChanged(srcPath string, ev *fsnotify.FileEvent) error {
+	// TODO: Analyze jade files and be more intelligent here?
+	// Only recompile the file at srcPath and any files that import it?
+	// For now, just recompile all jade.
+	if err := recompileAllForCompiler(j); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (j *JadeCompilerType) RemoveOld() error {
+	// Simply iterate through createdFiles and remove each of them
+	for _, path := range j.createdFiles {
+		if err := util.RemoveIfExists(path); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ compilers/jade_compiler_test.go
@@ -0,0 +1,83 @@
+package compilers
+
+import (
+	"github.com/albrow/scribble/config"
+	"github.com/albrow/scribble/test_util"
+	"github.com/albrow/scribble/util"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestJadePathMatch(t *testing.T) {
+	// Create a root path where all of our test files for this
+	// test will live
+	root := string(os.PathSeparator) + filepath.Join("tmp", "jade_compiler_paths")
+	defer func() {
+		// Remove everything after we're done
+		if err := util.RemoveAllIfExists(root); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Create a few files.
+	tmpPaths := []string{
+		filepath.Join(root, "index.jade"),
+		filepath.Join(root, "archive", "index.jade"),
+		filepath.Join(root, "_includes", "foot.jade"),
+		filepath.Join(root, "_templates", "base.jade"),
+		filepath.Join(root, "archive", "notice.txt"),
+		filepath.Join(root, ".cache", "index.jade"),
+	}
+	if err := util.CreateEmptyFiles(tmpPaths); err != nil {
+		t.Fatal(err)
+	}
+
+	// Only some paths are expected to be matched by the JadeCompiler,
+	// the other files should be ignored.
+	expectedPaths := []string{
+		filepath.Join(root, "index.jade"),
+		filepath.Join(root, "archive", "index.jade"),
+	}
+
+	// Use the MatchFunc to find all the paths
+	config.SourceDir = root
+	gotPaths, err := FindPaths(JadeCompiler.CompileMatchFunc())
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Check that the paths we get are correct
+	test_util.CheckStringsMatch(t, expectedPaths, gotPaths)
+}
+
+func TestJadeCompile(t *testing.T) {
+	// Create a root path where all of our test files for this
+	// test will live
+	root := string(os.PathSeparator) + filepath.Join("tmp", "test_jade_compiler")
+	defer func() {
+		// Remove everything after we're done
+		if err := util.RemoveAllIfExists(root); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Copy some files from test_files to source directory in the temp root
+	testFilesDir := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "albrow", "scribble", "test_files", "jade")
+	srcDir := filepath.Join(root, "source")
+	destDir := filepath.Join(root, "public")
+	if err := util.RecursiveCopy(filepath.Join(testFilesDir, "source"), srcDir); err != nil {
+		t.Fatal(err)
+	}
+
+	// Attempt to compile the html template files
+	config.SourceDir = filepath.Join(root, "source")
+	config.DestDir = filepath.Join(root, "public")
+	if err := JadeCompiler.Compile(filepath.Join(srcDir, "index.jade")); err != nil {
+		t.Fatal(err)
+	}
+
+	// Make sure the compiled result is correct
+	expectedDir := filepath.Join(testFilesDir, "public")
+	test_util.CheckFilesMatch(t, filepath.Join(expectedDir, "index.html"), filepath.Join(destDir, "index.html"))
+}
